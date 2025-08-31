2a50cf6c5fa902a0f3b151b1d412a8bbb25afe3d
abdelrahmans5 <abdosayco57@gmail.com>
2025-08-31 15:21:25 +0300

    router express

--- src/app/core/guards/auth-guard.ts
+++ src/app/core/guards/auth-guard.ts
@@ -1,6 +1,5 @@
 import { inject } from '@angular/core';
-import { CanActivateFn } from '@angular/router';
-import { Router } from 'express';
+import { CanActivateFn, Router } from '@angular/router';
 import { CookieService } from 'ngx-cookie-service';
 
 export const authGuard: CanActivateFn = (route, state) => {
